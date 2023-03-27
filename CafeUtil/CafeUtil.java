4b268d906dd26b6507e094074978e167d2be7749
Ronnie Rios <ronniesmail14@gmail.com>
2023-03-27 14:46:07 -0500

    added initial method

--- CafeUtil/CafeUtil.java
+++ CafeUtil/CafeUtil.java
@@ -3,4 +3,13 @@ public class CafeUtil {
     public static void main(String[] args) {
         
     }
+
+    public int getStreakGoal () {
+        int numWeeks = 10;
+        int sum = 0;
+        for (int i =1; i <= numWeeks; i++) {
+            sum += 1;
+        }
+        return sum;
+    }
 }
